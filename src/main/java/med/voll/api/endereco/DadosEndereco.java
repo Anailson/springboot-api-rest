97b904ae194f8b303711484676906cbbf6d41cc3
anailson ribeiro <arsantos315@gmail.com>
2022-11-12 20:22:34 -0300

    Entidades JPA

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -9,4 +9,5 @@ public record DadosEndereco(String logradouro, String bairro, String cep, String
  * Consulta sobre a utilização do record no java
  * https://www.guiadojava.com.br/2021/04/java-records.html
  * https://docs.oracle.com/en/java/javase/16/language/records.html
+ * o RECORD É SIMILAR AO DTO. PORÉM COM MENOS ATRIBUTOS
  */
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,22 @@
+package med.voll.api.endereco;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
