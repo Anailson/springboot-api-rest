8341bd41d5c0a2895c64183336bcc1b86b6a5cc0
anailson ribeiro <arsantos315@gmail.com>
2022-11-13 11:55:17 -0300

    Interfaces Repository

--- src/main/java/med/voll/api/medico/dadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/dadosCadastroMedico.java
@@ -5,3 +5,7 @@ import med.voll.api.endereco.DadosEndereco;
 public record dadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade,
                                   DadosEndereco endereco ) {
 }
+
+/**
+ * https://medium.com/experiencecode/usando-records-em-java-9afecf7495b3
+ */
