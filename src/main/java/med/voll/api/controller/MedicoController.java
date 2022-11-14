079d4055d54ca19b6e3b17fac40609f428f9da2e
anailson ribeiro <arsantos315@gmail.com>
2022-11-14 20:14:41 -0300

    Atualizando dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,9 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
-import med.voll.api.medico.dadosCadastroMedico;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,8 +33,9 @@ public class MedicoController {
 
 	@PutMapping
 	@Transactional
-	public void atualizar(@RequestBody  @Valid dadosCadastroMedico dados){
-
+	public void atualizar(@RequestBody  @Valid DadosAtualizacaoMedico dados){
+		var medico = repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
 	}
 
 
---
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,16 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+import javax.validation.constraints.NotNull;
+
+public record DadosAtualizacaoMedico(
+        @NotNull
+        Long id,
+
+        String nome,
+
+        String telefone,
+
+        DadosEndereco endereco) {
+}
