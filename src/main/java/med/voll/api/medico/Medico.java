c2dfc98144639bcc3997ac9a16f41df56d9d96c0
anailson ribeiro <arsantos315@gmail.com>
2022-11-17 09:57:50 -0300

    Exclusão Lógica

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -21,6 +21,8 @@ public class Medico {
     private String telefone;
     private String crm;
 
+    private Boolean ativo;
+
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
@@ -29,6 +31,7 @@ public class Medico {
 
 
     public Medico(dadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -48,5 +51,9 @@ public class Medico {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
 
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
