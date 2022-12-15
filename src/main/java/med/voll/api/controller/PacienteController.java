6b989ac404cc35eb17dbff4f1fd1816e7e603c61
anailson <arsantos315@gmail.com>
2022-12-15 19:54:31 -0300

    Pacientes - Requisições DELETE

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -38,4 +38,11 @@ public class PacienteController {
             paciente.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+
 }
