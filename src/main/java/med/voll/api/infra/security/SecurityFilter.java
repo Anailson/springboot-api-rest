c2b6342177c32badf78856735b831d9dd01f0d41
anailson <arsantos315@gmail.com>
2022-12-22 15:26:03 -0300

    Autenticação do usuário

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,6 +1,9 @@
 package med.voll.api.infra.security;
 
+import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,29 +19,37 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
        // System.out.println("FILTRO CHAMADO!!");
         //lógica para recuperar o TOKEN
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
-        System.out.println(subject);
-       // System.out.println(tokenJWT);
 
+        if(tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+         //   System.out.println(subject);
+            var usuario = repository.findByLogin(subject);
 
-
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        
         filterChain.doFilter(request,response);
-
 
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null){
-            throw  new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
-
+        if(authorizationHeader !=null){
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer", "");
+
+        return null;
+
     }
 }
