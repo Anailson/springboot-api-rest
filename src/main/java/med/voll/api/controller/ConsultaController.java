1026399852a5257f934a2a16dc29306f678bfb1d
anailson <arsantos315@gmail.com>
2023-04-03 19:10:45 -0300

    Classe service - criando a regra de negócio

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,7 +1,9 @@
 package med.voll.api.controller;
 
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,14 @@ import javax.validation.Valid;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultas agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
 
+        agenda.agendar(dados);
         //System.out.println(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
